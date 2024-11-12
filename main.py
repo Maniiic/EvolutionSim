8c23a070de6e5f82b1f79fe6d7660561c70e86b3
Maniiic <oliver.vaillantjones@gmail.com>
2024-11-12 00:25:33 +0000

    Eating Function

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ backgroundColour = (0, 0, 0)
 #Constants
 foodAmount = 5
 creatureAmount = 3
+null = pygame.Vector2(9999,9999)
 
 
 
@@ -40,31 +41,34 @@ class Creature(Entity):
     self.senseRange = 60
     self.colour = (255,255,255)
     self.path = randomVector()
-    self.closestFood = NULL
+    self.closestFood = null
+    self.vel = NULL
 
   def update(self):
-    self.closestFood = self.getClosestFoodInRange()
+    food = self.getClosestFoodInRange()
     self.updateVelocity()
     self.updatePosition()
-    #self.updateEating()
-
+    self.updateEating(food)
 
   def getClosestFoodInRange(self):
     smallest = math.inf
+    foodToReturn = NULL
     for food in foods:
       distance = self.pos.distance_to(food.pos)
       if distance < smallest:
         smallest = distance
         if distance <= self.senseRange:
-          return food.pos
-    return self.path
-
+          self.closestFood = food.pos
+          foodToReturn = food
+    if foodToReturn != NULL:
+      return foodToReturn
+    
 
   def updateVelocity(self):
-    
-    self.path = self.closestFood
+    if self.closestFood != null:
+      self.path = self.closestFood
     #Gives new path when it reaches destination
-    if self.pos.distance_to(self.path) <=20:
+    if self.pos.distance_to(self.path) <= 20:
       self.path = randomVector()
     #Sets velocity in direction of path
     self.vel = (self.path - self.pos).normalize()*self.speed*deltaTime
@@ -72,12 +76,22 @@ class Creature(Entity):
   def updatePosition(self):
     self.pos += self.vel
 
+  def updateEating(self, food):
+    if self.pos.distance_to(self.closestFood) <= 20:
+      food.delete()
+      self.closestFood = null
+
 
 
 class Food(Entity):
   def __init__(self):
     super().__init__()
     self.colour = (255,255,0)
+  
+  def delete(self):
+    foods.remove(self)
+
+
   
 
 
@@ -91,7 +105,7 @@ def randomVector():
 foods = [Food() for x in range(foodAmount)]
 creatures = [Creature() for x in range(creatureAmount)]
 
-
+print(foods)
 #Main Loop
 while run:
   surface.fill(backgroundColour)
